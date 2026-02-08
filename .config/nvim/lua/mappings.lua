c36f4e7abaf2e06e547e3c06750978f8d2776747
Diego Caspi <55052203+diegocaspi@users.noreply.github.com>
2026-02-08 11:23:12 +0100

    new mapping for early exit nvim (#2)

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
